3ea0dd5eddc8dad8a3b62171e92dbb4873860f1b
Ymugari <danich.a@inbox.ru>
2022-11-23 21:39:59 +0300

    feat(03):create README.md & create name.cpp & add changes name.cpp

---
+++ 03_cpp/intro/name.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+ 
+using namespace std;
+ 
+int main()
+{
+    char name[50];
+    cout << "What is your name?" << endl;
+    cin >> name;
+    cout << "Hello, " << name << endl;
+    return 0;
+}
